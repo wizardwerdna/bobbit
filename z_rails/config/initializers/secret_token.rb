b662a083f05c77505690f9eaf93aad06a9a52ee2
Andrew C. Greenberg <wizardwerdna@gmail.com>
2012-07-29 15:19:39 -0700

    initial install of rails

---
+++ z_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZRails::Application.config.secret_token = '6e3f7b731a1fc32512ff2040b34315a2a9b7043e7c2678921d5ee63a292e6cce6e3571047d3b939c9ea1ca568125d9478e145d593035afa01b90dc0190b71f16'
